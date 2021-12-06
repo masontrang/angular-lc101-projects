7101148a70db46ff0492bc03f69393a7b200f6ab
Mason <mt9260@gmail.com>
2021-12-06 16:44:42 -0600

    initial

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Tesla Photos';
+  image1 = 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2021-tesla-model-3-mmp-1-1602871915.jpg?crop=0.787xw:0.708xh;0.109xw,0.197xh&resize=240:*';
+  image2 = 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2019-tesla-model-3-long-range-103-1627583154.jpg?crop=1xw:1xh;center,top&resize=240:*';
+  image3 = 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2019-tesla-model-3-108-1574807027.jpg?crop=0.920xw:1.00xh;0.0801xw,0&resize=240:*';
 
   constructor() { }
 
