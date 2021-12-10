009f05f105e4c3911cff7e2776e38bf9c95aaafc
Mason Trang <mt9260@gmail.com>
2021-12-09 19:27:25 -0500

    update

--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -25,8 +25,15 @@ export class CandidatesComponent implements OnInit {
 
   // Code the addToCrew function here:
 
-
+addToCrew(person){
+  console.log("already here", person)
+  if (!this.crew.includes(person)){
+  this.crew.push(person);
+  }
+}
   // BONUS: Code the changeMissionName function here:
-
-
+changeMissionName(name: string){
+    this.missionName = name;
+    return false;
+}
 }
